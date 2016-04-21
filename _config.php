adfd1cc72fc067101ad9fee10c4fd296d2d781ce
Maxime Rainville <max@firebrand.co.nz>
2016-04-22 11:04:08 +1200

    Change the HAIL_DIR constanct to be dynamicaly define.

--- _config.php
+++ _config.php
@@ -1,4 +1,5 @@
 <?php
 
-define('HAIL_DIR', 'silverstripe-hail');
+define('HAIL_DIR', ltrim(Director::makeRelative(realpath(__DIR__)), DIRECTORY_SEPARATOR));
+
 define('HAIL_PATH', BASE_PATH . '/' . CMS_DIR);
