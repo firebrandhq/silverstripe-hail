c842b1c72a3d5fa734362c0a73238e6756ae59c0
Marc Espiard <marc@firebrand.nz>
2018-05-04 16:52:27 +1200

    Fixed bug with youtube lazy loading and infite scroll

--- client/dist/js/hail.bundle.js
+++ client/dist/js/hail.bundle.js
@@ -276,20 +276,18 @@ __webpack_require__(/*! ../styles/main.scss */ "./client/src/styles/main.scss");
 "use strict";
 
 
-var youtubeLazyLoaders = document.querySelectorAll(".hail-video-youtube");
-for (var i = 0; i < youtubeLazyLoaders.length; i++) {
-        youtubeLazyLoaders[i].addEventListener("click", function () {
+jQuery(function ($) {
+    $('#hail-container').on('click', '.hail-video-youtube', function () {
+        var iframe = document.createElement("iframe");
 
-                var iframe = document.createElement("iframe");
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "");
+        iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
 
-                iframe.setAttribute("frameborder", "0");
-                iframe.setAttribute("allowfullscreen", "");
-                iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
-
-                this.innerHTML = "";
-                this.appendChild(iframe);
-        });
-}
+        $(this).html("");
+        $(this).append(iframe);
+    });
+});
 
 /***/ }),
 
--- client/src/js/youtube-lazy-loading.js
+++ client/src/js/youtube-lazy-loading.js
@@ -1,14 +1,12 @@
-var youtubeLazyLoaders = document.querySelectorAll(".hail-video-youtube");
-for (var i = 0; i < youtubeLazyLoaders.length; i++) {
-    youtubeLazyLoaders[i].addEventListener("click", function () {
-
+jQuery(function ($) {
+    $('#hail-container').on('click', '.hail-video-youtube', function () {
         var iframe = document.createElement("iframe");
 
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("allowfullscreen", "");
         iframe.setAttribute("src", "https://www.youtube.com/embed/" + this.dataset.embed + "?rel=0&showinfo=0&autoplay=1");
 
-        this.innerHTML = "";
-        this.appendChild(iframe);
+        $(this).html("");
+        $(this).append(iframe);
     });
-}
+});
