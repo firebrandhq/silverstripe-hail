9f6523a7f0beee36c52f52918f826a340966f882
Marc Espiard <marc@firebrand.nz>
2019-02-14 10:04:41 +1300

    Fixed featured article being imported a second time and crash on settings page

--- code/Admin/HailExcludedListExtension.php
+++ code/Admin/HailExcludedListExtension.php
@@ -3,26 +3,31 @@
 class HailExcludedListExtension extends DataExtension
 {
 
-    private static $db = array(
+    private static $db = [
         'HailExcludedTagIDs' => 'Varchar(255)',
-    );
+    ];
 
     public function updateCMSFields(FieldList $fields)
     {
-//        $fields = parent::updateCMSFields($fields);
+        $orgs = [];
 
         $orgs = HailOrganisation::get();
-        $tags = array();
+
+        $tags = [];
         foreach ($orgs as $org) {
             //To avoid errors when you authorize the app with Hail
-            if($org->HailOrgID) {
-                $tags = array_merge(HailApi::getPrivateTagList($org), $tags);
+            if ($org->HailOrgID) {
+                try {
+                    $tags = array_merge(HailApi::getPrivateTagList($org), $tags);
+                } catch (Exception $exception) {
+                    //Fail silently
+                }
             }
         }
         //Check if at least 1 organisation is authorized
-        if(sizeof($tags) > 0) {
-            $fields->addFieldToTab('Root.Hail',Tab::create('ExcludedTags', 'Excluded Private Tags'));
-            $tagField = ListboxField ::create('HailExcludedTagIDs', 'Hail Private Tags to exclude:', $tags)->setEmptyString('None')->setMultiple(true);
+        if (sizeof($tags) > 0) {
+            $fields->addFieldToTab('Root.Hail', Tab::create('ExcludedTags', 'Excluded Private Tags'));
+            $tagField = ListboxField::create('HailExcludedTagIDs', 'Hail Private Tags to exclude:', $tags)->setEmptyString('None')->setMultiple(true);
             $fields->addFieldToTab('Root.Hail.ExcludedTags', $tagField);
 
         }
--- code/HailObjects/HailPublication.php
+++ code/HailObjects/HailPublication.php
@@ -1,90 +1,100 @@
 <?php
 
-class HailPublication extends HailApiObject {
-	
-	private static $db = array(
-		'Title' => 'Varchar',
-		'Editorial' => 'HTMLText',
-		'DueDate' => 'Datetime',
-		'Status' => 'Text',
-		'Style' => 'Text',
-		'Created' => 'Datetime',
-		'Url' => 'Text',
-	);
-	
-	private static $has_one = array(
-		'FeaturedArticle' => 'HailArticle',
-		'HeroImage' => 'HailImage'
-	);
-	
-	private static $searchable_fields = array(
-		'Title','Editorial','Created','Style'
-	);
-	
-	private static $summary_fields = array(
-		'Organisation.Title' => 'Hail Organisation',
-		'HailID' => 'Hail ID',
-		'Title' => 'Title',
-		'Editorial' => 'Editorial',
-		'Style' => 'Style',
-		'Fetched' => 'Fetched'
-	);
-	
-	protected static function getObjectType() {
-		return HailApi::PUBLICATIONS;
-	}
-	
-	protected function importing($data) {
-		if (!empty($data->style)) {
-			$this->Style = empty($data->style->name) ? '' : $data->style->name ;
-		}
-		
-		$this->processFeaturedArticle($data->featured_article);
-	}
-	
-	protected static function apiMap() {
-		return array(
-			'Title' => 'title',
-			'Editorial' => 'editorial',
-			'DueDate' => 'due_date',
-			'Status' => 'status',
-			'Created' => 'created_date',
-			'Url' => 'url',
-		);
-	}
-	
-	public function getCMSFields( ) {
-		$fields = parent::getCMSFields();
-		
+class HailPublication extends HailApiObject
+{
 
-		return $fields;
-	}
-	
-		// Match the featured article if there's one
-	private function processFeaturedArticle($articleData) {
-		if ($articleData) {
-			$article = HailArticle::get()->filter(array('HailID' => $articleData->id))->first();
-			if (!$article) {
-				$article = new HailArticle();
-			}
-			$article->importHailData($articleData);
-			
-			$heroImage = $article->HeroImageID;
-			
-			$article = $article->ID;
-		} else {
-			$article = null;
-		}
-		
-		$this->FeaturedArticleID = $article;
-		
-		if (isset($heroImage) && $heroImage) {
-			$this->HeroImageID = $heroImage;
-		}
-		
-	}
-	
-	public function forTemplate() {
-		return $this->renderWith('HailPublication', array('HailHolder' => Controller::curr()) );
-	}
+    private static $db = [
+        'Title' => 'Varchar',
+        'Editorial' => 'HTMLText',
+        'DueDate' => 'Datetime',
+        'Status' => 'Text',
+        'Style' => 'Text',
+        'Created' => 'Datetime',
+        'Url' => 'Text',
+    ];
+
+    private static $has_one = [
+        'FeaturedArticle' => 'HailArticle',
+        'HeroImage' => 'HailImage'
+    ];
+
+    private static $searchable_fields = [
+        'Title',
+        'Editorial',
+        'Created',
+        'Style'
+    ];
+
+    private static $summary_fields = [
+        'Organisation.Title' => 'Hail Organisation',
+        'HailID' => 'Hail ID',
+        'Title' => 'Title',
+        'Editorial' => 'Editorial',
+        'Style' => 'Style',
+        'Fetched' => 'Fetched'
+    ];
+
+    protected static function getObjectType()
+    {
+        return HailApi::PUBLICATIONS;
+    }
+
+    protected function importing($data)
+    {
+        if (!empty($data->style)) {
+            $this->Style = empty($data->style->name) ? '' : $data->style->name;
+        }
+
+        $this->processFeaturedArticle($data->featured_article);
+    }
+
+    protected static function apiMap()
+    {
+        return [
+            'Title' => 'title',
+            'Editorial' => 'editorial',
+            'DueDate' => 'due_date',
+            'Status' => 'status',
+            'Created' => 'created_date',
+            'Url' => 'url',
+        ];
+    }
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+
+        return $fields;
+    }
+
+    // Match the featured article if there's one
+    private function processFeaturedArticle($articleData)
+    {
+        if ($articleData) {
+            $article = HailArticle::get()->filter(['HailID' => $articleData->id])->first();
+            if (!$article) {
+                $article = new HailArticle();
+                $article->importHailData($articleData);
+            }
+
+            $heroImage = $article->HeroImageID;
+
+            $article = $article->ID;
+        } else {
+            $article = null;
+        }
+
+        $this->FeaturedArticleID = $article;
+
+        if (isset($heroImage) && $heroImage) {
+            $this->HeroImageID = $heroImage;
+        }
+
+    }
+
+    public function forTemplate()
+    {
+        return $this->renderWith('HailPublication', ['HailHolder' => Controller::curr()]);
+    }
 }
