2d6267b321593bc0471b41dbaafd2219a23ec90e
Greg Parsons <greg@firebrand.nz>
2023-02-13 19:19:29 +0000

    Update columns to allow larger tokens

--- code/CommsLayer/HailProvider.php
+++ code/CommsLayer/HailProvider.php
@@ -8,29 +8,29 @@ class HailProvider extends League\OAuth2\Client\Provider\AbstractProvider {
 		if (! array_key_exists('redirectUri', $options)) {
 			$options['redirectUri'] = static::getRedirectUri($org);
 		}
-		
+
 		if (!array_key_exists('clientId', $options)) {
 			$options['clientId'] = $org->HailClientID;
 		}
-		
+
 		if (!array_key_exists('clientSecret', $options)) {
 			$options['clientSecret'] = $org->HailClientSecret;
 		}
-		
+
 		if (!array_key_exists('scopes', $options)) {
 			$options['scopes'] = ['user.basic content.read'];
 		}
-		
+
 		parent::__construct($options);
 	}
-	
+
 	/**
 	 * Gets the redirect callback for Hail
-	 * @param HailOrganisation $org The Hail organisation 
+	 * @param HailOrganisation $org The Hail organisation
 	 * @return string
 	 */
 	public static function getRedirectUri($org) {
-		
+
 		// If only one Hail organisation exists, then don't suffix the org idate
 		if(HailOrganisation::get()->count() > 1) {
 			return Director::absoluteURL('HailCallbackController', true) . '?org=' . $org->ID;
@@ -38,26 +38,26 @@ class HailProvider extends League\OAuth2\Client\Provider\AbstractProvider {
 			return Director::absoluteURL('HailCallbackController', true);
 		}
 	}
-	
+
 	public function urlAuthorize() {
 		return 'https://hail.to/oauth/authorise';
-	} 
-	
-	public function urlAccessToken() {		
+	}
+
+	public function urlAccessToken() {
 		return 'https://hail.to/api/v1/oauth/access_token';
 	}
-	
+
 	public function urlUserDetails(\League\OAuth2\Client\Token\AccessToken $token) {
 		return HailApi::config()->Url . 'me';
 	}
-	
+
 	public function userDetails($response, \League\OAuth2\Client\Token\AccessToken $token) {
 		return $response;
 	}
-	
+
 	public static function getHailAccessToken(HailOrganisation $org) {
 		$siteconfig = SiteConfig::current_site_config();
-		
+
 		if (! static::isAuthorised($org)) {
 			throw new HailApiException('(Organisation: ' .$org->Title . ') - Need to reauthorize SilverStripe to access Hail.');
 		}
@@ -71,30 +71,30 @@ class HailProvider extends League\OAuth2\Client\Provider\AbstractProvider {
 			try {
 				$provider = new static($org);
 				$grant = new \League\OAuth2\Client\Grant\RefreshToken();
-				$token = $provider->getAccessToken(
-					$grant, 
+				$token = $org->getAccessToken(
+					$grant,
 					['refresh_token' => $org->HailRefreshToken]
 				);
 			} catch (Exception $ex) {
 				throw new HailApiException('(Organisation: ' .$org->Title . ') - Need to reauthorize SilverStripe to access Hail.');
 			}
-			
+
 			$org->HailAccessToken = $token->accessToken;
 			$org->HailAccessTokenExpire = $token->expires;
 			$org->HailRefreshToken = $token->refreshToken;
-			
+
 			$org->write();
 		}
 
 		return $org->HailAccessToken;
 	}
-	
+
 	public static function isAuthorised(HailOrganisation $org) {
 		return $org->HailAccessTokenExpire && $org->HailAccessToken && $org->HailRefreshToken;
 	}
-	
+
 	public static function isReadyToAuthorised(HailOrganisation $org) {
 		return $org->HailClientID && $org->HailClientSecret;
 	}
-	
+
 }
